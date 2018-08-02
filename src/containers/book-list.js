3aeabe8d648999a71b77d149a1f151a34b51d0b9
raven920 <raven@ideashock.net>
2018-08-02 16:49:34 -0500

    Aplicacion finalizada

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
+import {selectBook} from '../actions/index';
+import {bindActionCreators} from 'redux';
 
 class BookList extends Component{
     renderList(){
         return this.props.books.map((book) => {
             return (
-                <li key={book.title} className="list-group-item">{book.title}</li>
+                <li 
+                    key={book.title}
+                    onClick={() => this.props.selectBook(book)}
+                    className="list-group-item">
+                        {book.title}
+                </li>
             )
         });
     }
@@ -27,4 +34,13 @@ function mapStateToProps(state){
     };
 }
 
-export default connect(mapStateToProps)(BookList)
+//Anything returned from this function will end up as props on the BookList container
+function mapDispatchToProps(dispatch){
+    //Whenever selectBook is called, the result should be passed to all of reducers
+    return bindActionCreators({selectBook: selectBook}, dispatch);
+}
+
+//Promote BookList from a component to a container - it needs to know
+//about this new tdispatch method, selectBook. Make it availkable
+// as a prop
+export default connect(mapStateToProps, mapDispatchToProps)(BookList)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from '../../../../.cache/typescript/2.9/node_modules/redux';
 import BooksReducer from './reducer_books';
+import ActiveBook from './reducer_active_book';
 
 const rootReducer = combineReducers({
-  books: BooksReducer
+  books: BooksReducer,
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
